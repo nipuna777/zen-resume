3f26fd5e7935baccbf02c9ab9ff0ce125a11cc0f
Nipuna Gunathilake <nipuna@ridebeam.com>
2020-08-10 23:24:34 +0800

    added image uploader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css';
 import 'react-quill/dist/quill.snow.css';
+import { CloudinaryContext } from 'cloudinary-react';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <CloudinaryContext cloudName="dtmkcgalp">
+            <Component {...pageProps} />
+        </CloudinaryContext>
+    );
 }
 
 export default MyApp;
