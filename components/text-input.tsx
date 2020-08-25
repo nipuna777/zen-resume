257894141353a08614f7334207e11e2abf1664f7
Nipuna Gunathilake <nipuna777@gmail.com>
2020-08-25 08:11:23 +0800

    Develop (#4)
    * style fixes and default profile picture change
    
    * improve image upload error handeling

--- components/text-input.tsx
+++ components/text-input.tsx
@@ -1,8 +1,14 @@
 export default function TextInput({ label, name, inputRef }) {
     return (
-        <label style={{ display: 'flex', flexDirection: 'column', width: '100%', marginBottom: 10 }}>
-            {label}
-            <input name={name} ref={inputRef} />
-        </label>
+        <div className="mb-5">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+                {label}
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    name={name}
+                    ref={inputRef}
+                />
+            </label>
+        </div>
     );
 }
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -4,6 +4,7 @@ import TextInput from '../components/text-input';
 import TextAreaInput from '../components/text-area-input';
 import { Image, Transformation } from 'cloudinary-react';
 import { useState } from 'react';
+import LoadingSVG from '../public/images/loading.svg';
 
 let ReactQuill;
 if (typeof window !== 'undefined') {
@@ -30,52 +31,71 @@ export default function Resume() {
                 'To excel as a professional, in the fields of Accountancy and financial management while delivering best service possible in achieving organizational objectives.',
         },
     });
-    const [profileImagePublicId, setProfileImagePublicId] = useState('sample');
+    const [profileImagePublicId, setProfileImagePublicId] = useState('placeholder-profile_ubymfr');
+    const [isLoading, setIsLoading] = useState(false);
     const sections = [0, 1];
 
     const fieldValues = watch();
     const { title, telephone, email, address, imageUrl } = fieldValues;
     return (
         <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <form style={{ width: 555, margin: 10 }}>
+            {isLoading && (
+                <div className="flex absolute bottom-0 left-0 bg-gray-500 bg-opacity-25 w-screen h-screen">
+                    <LoadingSVG />
+                </div>
+            )}
+            <form className="p-5 h-screen overflow-y-auto overflow-x-hidden bg-gray-200 bg-opacity-25">
+                <h1 className="text-2xl">Zen Resume</h1>
+                <p className="text-xs">Edit your resume by changing settings below</p>
+                <hr className="mb-5 mt-5" />
+
                 <TextInput label="Title" inputRef={register} name="title" />
-                <label style={{ display: 'flex', flexDirection: 'column', width: '100%', marginBottom: 10 }}>
-                    Select Image (Maximum 10mb)
-                </label>
-                <input
-                    type="file"
-                    name="profile-image"
-                    accept="image/*"
-                    onChange={(event) => {
-                        console.log(event.target.files[0]);
-                        let selectedFile = event.target.files[0];
 
-                        var data = new FormData();
-                        data.append('file', selectedFile);
-                        data.append('upload_preset', 'smkrczl7');
+                <div className="mb-5">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">Select Image (Maximum 10mb)</label>
+                    <input
+                        type="file"
+                        name="profile-image"
+                        accept="image/*"
+                        onChange={(event) => {
+                            let selectedFile = event.target.files[0];
+                            const mbFileSize = event.target.files[0].size / (1024 * 1024);
+                            if (mbFileSize > 10) {
+                                alert('Image size too large. Must be no larger than 10MB');
+                                return;
+                            }
 
-                        fetch('https://api.cloudinary.com/v1_1/dtmkcgalp/upload', {
-                            method: 'POST',
-                            body: data,
-                        })
-                            .then((res) => res.json())
-                            .then((res) => {
-                                if (res.error) {
-                                    console.error(res.error);
-                                    const message = res.error.message || 'Error uploading image';
-                                    alert(message);
-                                } else {
-                                    console.log(res.public_id);
-                                    setProfileImagePublicId(res.public_id);
-                                }
+                            var data = new FormData();
+                            data.append('file', selectedFile);
+                            data.append('upload_preset', 'smkrczl7');
+
+                            setIsLoading(true);
+                            fetch('https://api.cloudinary.com/v1_1/dtmkcgalp/upload', {
+                                method: 'POST',
+                                body: data,
                             })
-                            .catch((error) => {
-                                console.error(error);
-                                const message = error.message || 'Error uploading image';
-                                alert(message);
-                            });
-                    }}
-                />
+                                .then((res) => res.json())
+                                .then((res) => {
+                                    if (res.error) {
+                                        console.error(res.error);
+                                        const message = res.error.message || 'Error uploading image';
+                                        alert(message);
+                                    } else {
+                                        console.log(res.public_id);
+                                        setProfileImagePublicId(res.public_id);
+                                    }
+                                })
+                                .catch((error) => {
+                                    console.error(error);
+                                    const message = error.message || 'Error uploading image';
+                                    alert(message);
+                                })
+                                .finally(() => {
+                                    setIsLoading(false);
+                                });
+                        }}
+                    />
+                </div>
 
                 <TextInput label="Email" inputRef={register} name="telephone" />
                 <TextInput label="Telephone" inputRef={register} name="email" />
@@ -83,21 +103,24 @@ export default function Resume() {
 
                 {sections.map((section, i) => {
                     return (
-                        <>
+                        <div className="mb-4 mt-4">
                             <TextInput label={`Section ${i} title`} inputRef={register} name={`sectionTitle${i}`} />
                             {ReactQuill ? (
-                                <Controller
-                                    as={ReactQuill}
-                                    name={`sectionValue${i}`}
-                                    control={control}
-                                    defaultValue=""
-                                />
+                                <label className="block text-gray-700 text-sm font-bold mb-2">
+                                    Section {i}
+                                    <Controller
+                                        as={ReactQuill}
+                                        name={`sectionValue${i}`}
+                                        control={control}
+                                        defaultValue=""
+                                    />
+                                </label>
                             ) : null}
-                        </>
+                        </div>
                     );
                 })}
             </form>
-            <div className={styles.container}>
+            <div className="flex flex-col w-full p-8 border-l-2 border-gray-300">
                 <header className={styles.header}>
                     <h1 className={styles.headerTitle}>{title}</h1>
                     <div className={styles.headerContent}>
