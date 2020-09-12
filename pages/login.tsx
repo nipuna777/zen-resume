1e4afc3b1a3d95df8ddca7fad47d790a6b3ab71c
Nipuna Gunathilake <nipuna777@gmail.com>
2020-09-12 13:31:43 +0800

    fix re-direct

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,7 +8,7 @@ const uiConfig = {
     // Popup signin flow rather than redirect flow.
     signInFlow: 'popup',
     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: 'https://zen-resume.firebaseapp.com/__/auth/handler',
+    signInSuccessUrl: '/resume',
     // We will display Google and Facebook as auth providers.
     signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID, firebase.auth.EmailAuthProvider.PROVIDER_ID],
 };
